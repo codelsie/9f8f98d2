5db4c42ccf5923a4e4a4d3a958d7877eaa41faa7
codelsie <lizzyogb1@gmail.com>
2025-02-26 14:03:09 +0100

    done

--- script.js
+++ script.js
@@ -1,9 +1,65 @@
 "use strict";
 
+class AdManager
+{
+    getBanner465x60() {
+        return `<script type="text/javascript">
+            atOptions = {
+                'key' : 'd52fb4d960ddefdbd55ba3a5f712ea48',
+                'format' : 'iframe',
+                'height' : 60,
+                'width' : 468,
+                'params' : {}
+            };
+        </script>
+        <script type="text/javascript" src="//www.highperformanceformat.com/d52fb4d960ddefdbd55ba3a5f712ea48/invoke.js"></script>`;
+    }
+
+    getBanner300x250() {
+        return `<script type="text/javascript">
+            atOptions = {
+                'key' : 'ae8a5226225aa195612b3a3625df792b',
+                'format' : 'iframe',
+                'height' : 250,
+                'width' : 300,
+                'params' : {}
+            };
+        </script>
+        <script type="text/javascript" src="//www.highperformanceformat.com/ae8a5226225aa195612b3a3625df792b/invoke.js"></script>`;
+    }
+
+    getNativeBanner() {
+        return `<script async="async" data-cfasync="false" src="//pl25973424.effectiveratecpm.com/2df87e8d1e230437faac8785f929ab17/invoke.js"></script>
+        <div id="container-2df87e8d1e230437faac8785f929ab17"></div>`;
+    }
+
+    getAsElements(script) {
+        let container = document.createElement('div');
+        container.innerHTML = script;
+        return container.children;
+    }
+}
+
 class PageManager {
     constructor() {
-        this.pageBody = $('#page_body');
-        console.log(this)
+        this.adManager = new AdManager();
+        this.buildDocument();
+        this.appendToStart();
+    }
+
+    buildDocument() {
+        this.fragment = {};
+        this.fragment.main = document.querySelector('#main'); 
+        this.fragment.postBody = this.fragment.main.querySelector('.post-body');
+    }
+
+    appendToStart() {
+        let container = document.createElement('div');
+        this.fragment.postBody.appendChild(container);
+        let banner = this.adManager.getAsElements(this.adManager.getBanner465x60());
+        for (let el of banner) {
+            container.appendChild(el);
+        }
     }
 }
 
